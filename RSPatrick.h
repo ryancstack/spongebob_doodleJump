c2fbd3b682ff297df13ff4aa5da2b6f0b0b2d270
ryancstack <ryancstack@me.com>
2013-04-25 02:14:55 -0700

    bug fixes to skeleton

--- RSPatrick.h
+++ RSPatrick.h
@@ -21,13 +21,6 @@ class RSPatrick : RSObject
 {
 public:
 	RSPatrick(QPixmap *pic, MainWindow *window, int xloc, int yloc, int xV, int yV);
-private:
-   int x_;
-   int y_;
-   int velocityX_;
-   int velocityY_;
-   QPixmap *picture_;
-   MainWindow *window_;
 };
 
 #endif /* defined(____RSPatrick__) */
