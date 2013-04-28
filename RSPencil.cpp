b63e5db853bc774b1b9162b6b3c8a6c9260ca21c
ryancstack <ryancstack@me.com>
2013-04-28 16:05:19 -0700

    deleting unused pictures

--- RSPencil.cpp
+++ RSPencil.cpp
@@ -8,3 +8,13 @@
 
 #include "RSSpongebob.h"
 #include "RSPencil.h"
+
+RSPencil::RSPencil(RSSpongebob *spongebob, QPixmap *pic, MainWindow *window, int xloc, int yloc) : RSObject(pic, window, xloc, yloc)
+{
+	spongebob_ = spongebob;
+}
+
+void RSPencil::move()
+{
+
+}
